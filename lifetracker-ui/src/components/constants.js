e5fd7320d105e464789855fbcb4ac136d89c2a13
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-07-18 15:33:18 -0400

    project 5 core features done

--- lifetracker-ui/src/components/constants.js
+++ lifetracker-ui/src/components/constants.js
@@ -1,5 +1,5 @@
 const DEVELOPMENT_API_BASE_URL = "http://localhost:3001"
-const PRODUCTION_API_BASE_URL = "https://lifetracker-api-vm.herokuapp.com/"
+const PRODUCTION_API_BASE_URL = "https://lifetracker-api-vm.herokuapp.com"
 const API_BASE_URL = PRODUCTION_API_BASE_URL
 
 export {API_BASE_URL}
